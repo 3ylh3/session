861e7d8040607741613f7db58fa0b53c30094b94
3ylh3 <781706889@qq.com>
2023-02-07 10:37:53 +0800

    first commit

---
+++ etcd/etcdSession.go
@@ -0,0 +1,83 @@
+package etcd
+
+import (
+	"context"
+	"fmt"
+	"github.com/3ylh3/session"
+	"go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/client/v3/concurrency"
+	"sync"
+	"time"
+)
+
+type EtcdStorageServer struct {
+	client *clientv3.Client
+}
+
+func (e *EtcdStorageServer) InitServer(url string) error {
+	// 初始化etcd client
+	var config = clientv3.Config{
+		Endpoints:   []string{url},
+		DialTimeout: 5 * time.Second,
+	}
+	client, err := clientv3.New(config)
+	if err != nil {
+		return err
+	}
+	e.client = client
+	return nil
+}
+
+func (e *EtcdStorageServer) Close() error {
+	return e.client.Close()
+}
+
+func (e *EtcdStorageServer) Put(key string, data string) error {
+	kv := clientv3.NewKV(e.client)
+	_, err := kv.Put(context.TODO(), key, data, clientv3.WithPrevKV())
+	return err
+}
+
+func (e *EtcdStorageServer) Get(key string) (string, error) {
+	kv := clientv3.NewKV(e.client)
+	resp, err := kv.Get(context.TODO(), key)
+	if err != nil {
+		return "", err
+	}
+	if 0 == resp.Count {
+		return "", fmt.Errorf("not find key:%s\n", key)
+	}
+	return string(resp.Kvs[0].Value), nil
+}
+
+func (e *EtcdStorageServer) Delete(key string) error {
+	kv := clientv3.NewKV(e.client)
+	_, err := kv.Delete(context.TODO(), key)
+	return err
+}
+
+func (e *EtcdStorageServer) ListKeysByPrefix(prefix string) ([]string, error) {
+	kv := clientv3.NewKV(e.client)
+	resp, err := kv.Get(context.TODO(), prefix, clientv3.WithPrefix())
+	if err != nil {
+		return nil, err
+	}
+	var result []string
+	for _, value := range resp.Kvs {
+		result = append(result, string(value.Key))
+	}
+	return result, nil
+}
+
+func (e *EtcdStorageServer) NewLocker(key string) (sync.Locker, error) {
+	s, err := concurrency.NewSession(e.client, concurrency.WithTTL(1))
+	if err != nil {
+		return nil, nil
+	}
+	lock := concurrency.NewLocker(s, key)
+	return lock, nil
+}
+
+func init() {
+	session.Register("etcd", &EtcdStorageServer{})
+}
---
+++ session.go
@@ -0,0 +1,316 @@
+package session
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/google/uuid"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+var (
+	StorageServersMu sync.RWMutex
+	StorageServers   = make(map[string]StorageServer)
+)
+
+const (
+	GET_SESSION_ERROR int = 1
+	SESSION_NOT_FIND  int = 2
+	SESSION_EXPIRED   int = 3
+	LOCK_ERROR        int = 4
+)
+
+// Session Session结构体,对外暴露操作方法
+type Session struct {
+	StorageServer StorageServer
+	ticker        *time.Ticker
+}
+
+// SessionInfo session公共父实体
+type SessionInfo struct {
+	// JSESSIONID
+	JSESSIONID string `json:"JSESSIONID"`
+	// 创建时间
+	CreateTime string `json:"createTime"`
+	// 过期时间
+	ExpireTime string `json:"expireTime"`
+}
+
+// StorageServer 接口，屏蔽session存储服务端增删改查具体实现
+type StorageServer interface {
+	InitServer(url string) error
+	Close() error
+	Put(key string, data string) error
+	Get(key string) (string, error)
+	Delete(key string) error
+	ListKeysByPrefix(prefix string) ([]string, error)
+	NewLocker(key string) (sync.Locker, error)
+}
+
+type SessionError struct {
+	ErrorCode    int
+	ErrorMessage string
+}
+
+func (s SessionError) Error() string {
+	return s.ErrorMessage
+}
+
+// Register 注册StorageServer实现类
+func Register(name string, server StorageServer) {
+	StorageServersMu.Lock()
+	defer StorageServersMu.Unlock()
+	if server == nil {
+		panic("Register storage server is null")
+	}
+	if _, exist := StorageServers[name]; exist {
+		panic("Register called twice for storage server type " + name)
+	}
+	StorageServers[name] = server
+}
+
+// Init 初始化
+func Init(name string, url string) (*Session, error) {
+	StorageServersMu.RLock()
+	defer StorageServersMu.RUnlock()
+	server, ok := StorageServers[name]
+	if !ok {
+		return nil, fmt.Errorf("unknow storage server %s,forgotten import?\n", name)
+	}
+	err := server.InitServer(url)
+	if err != nil {
+		return nil, fmt.Errorf("init error:%v\n", err)
+	}
+	// 定时每小时检查是否有异常状态的任务
+	ticker := time.NewTicker(1 * time.Minute)
+	go checkSession(ticker, server)
+	// 捕获信号，在异常退出时释放资源
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	s := &Session{
+		StorageServer: server,
+		ticker:        ticker,
+	}
+	go release(s, c)
+	return s, nil
+}
+
+// Create 创建新session
+func (s *Session) Create(expire int64) (string, error) {
+	// 生成JSESSIONID
+	id, err := uuid.NewUUID()
+	if err != nil {
+		return "", fmt.Errorf("generate JSESSIONID error:%v\n", err)
+	}
+	// 计算过期时间
+	createTime := time.Now()
+	expireUnix := createTime.Unix() + expire
+	expireTime := time.Unix(expireUnix, 0)
+	sessionInfo := &SessionInfo{
+		JSESSIONID: id.String(),
+		CreateTime: createTime.Format("2006-01-02 15:04:05"),
+		ExpireTime: expireTime.Format("2006-01-02 15:04:05"),
+	}
+	// 解析对象
+	bytes, err := json.Marshal(sessionInfo)
+	if err != nil {
+		return "", err
+	}
+	err = s.StorageServer.Put("/session/"+id.String(), string(bytes))
+	if err != nil {
+		return "", err
+	}
+	return id.String(), nil
+}
+
+// Get 获取session信息
+func (s *Session) Get(JSESSIONID string) (string, error) {
+	if JSESSIONID == "" {
+		return "", SessionError{
+			ErrorCode:    GET_SESSION_ERROR,
+			ErrorMessage: "JSESSIONID is null",
+		}
+	}
+	sessionStr, err := s.StorageServer.Get("/session/" + JSESSIONID)
+	if err != nil {
+		return "", SessionError{
+			ErrorCode:    GET_SESSION_ERROR,
+			ErrorMessage: err.Error(),
+		}
+	}
+	if sessionStr == "" {
+		return "", SessionError{
+			ErrorCode:    SESSION_NOT_FIND,
+			ErrorMessage: "session not find",
+		}
+	}
+	// 判断session是否过期
+	sessionInfo := &SessionInfo{}
+	err = json.Unmarshal([]byte(sessionStr), sessionInfo)
+	if err != nil {
+		return "", SessionError{
+			ErrorCode:    GET_SESSION_ERROR,
+			ErrorMessage: err.Error(),
+		}
+	}
+	expireTime, err := time.ParseInLocation("2006-01-02 15:04:05", sessionInfo.ExpireTime, time.Local)
+	if err != nil {
+		return "", SessionError{
+			ErrorCode:    GET_SESSION_ERROR,
+			ErrorMessage: "parse expire time error",
+		}
+	}
+	if time.Now().Unix() >= expireTime.Unix() {
+		// session过期
+		// 异步删除session信息
+		go s.StorageServer.Delete("/session/" + JSESSIONID)
+		return "", SessionError{
+			ErrorCode:    SESSION_EXPIRED,
+			ErrorMessage: "session is expired",
+		}
+	}
+	return sessionStr, nil
+}
+
+// Put 向session中添加内容
+func (s *Session) Put(JSESSIONID string, key string, value interface{}) error {
+	// 加分布式锁
+	lock, err := s.StorageServer.NewLocker("/session/" + JSESSIONID)
+	if err != nil {
+		return &SessionError{ErrorCode: LOCK_ERROR, ErrorMessage: "lock error:" + err.Error()}
+	}
+	lock.Lock()
+	defer lock.Unlock()
+	// 获取session信息
+	sessionStr, err := s.Get(JSESSIONID)
+	if err != nil {
+		return err
+	}
+	m := make(map[string]interface{})
+	err = json.Unmarshal([]byte(sessionStr), &m)
+	if err != nil {
+		return err
+	}
+	// 向session中添加内容
+	m[key] = value
+	result, err := json.Marshal(m)
+	if err != nil {
+		return err
+	}
+	// 存入后端存储系统
+	err = s.StorageServer.Put("/session/"+JSESSIONID, string(result))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Remove 移除session中内容
+func (s *Session) Remove(JSESSIONID string, key string) error {
+	// 加分布式锁
+	lock, err := s.StorageServer.NewLocker("/session/" + JSESSIONID)
+	if err != nil {
+		return &SessionError{ErrorCode: LOCK_ERROR, ErrorMessage: "lock error:" + err.Error()}
+	}
+	lock.Lock()
+	defer lock.Unlock()
+	// 获取session信息
+	sessionStr, err := s.Get(JSESSIONID)
+	if err != nil {
+		return err
+	}
+	m := make(map[string]interface{})
+	err = json.Unmarshal([]byte(sessionStr), &m)
+	if err != nil {
+		return err
+	}
+	// 删除session中内容
+	delete(m, key)
+	result, err := json.Marshal(m)
+	if err != nil {
+		return err
+	}
+	// 存入后端存储系统
+	err = s.StorageServer.Put("/session/"+JSESSIONID, string(result))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Delete 删除session
+func (s *Session) Delete(JSESSIONID string) error {
+	lock, err := s.StorageServer.NewLocker("/session/" + JSESSIONID)
+	if err != nil {
+		return &SessionError{ErrorCode: LOCK_ERROR, ErrorMessage: "lock error:" + err.Error()}
+	}
+	lock.Lock()
+	defer lock.Unlock()
+	return s.StorageServer.Delete("/session/" + JSESSIONID)
+}
+
+// Close 关闭
+func (s *Session) Close() error {
+	// 停止定时ticker
+	s.ticker.Stop()
+	// 关闭storage server
+	err := s.StorageServer.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// 定时检查
+func checkSession(ticker *time.Ticker, server StorageServer) {
+	for {
+		<-ticker.C
+		doCheckSession(server)
+	}
+}
+
+// 检查session是否过期
+func doCheckSession(server StorageServer) {
+	keys, err := server.ListKeysByPrefix("/session/")
+	if err != nil {
+		return
+	}
+	for _, key := range keys {
+		// 获取session信息
+		sessionStr, err := server.Get(key)
+		if err != nil {
+			continue
+		}
+		// 判断session是否过期
+		sessionInfo := &SessionInfo{}
+		err = json.Unmarshal([]byte(sessionStr), sessionInfo)
+		if err != nil {
+			continue
+		}
+		expireTime, err := time.ParseInLocation("2006-01-02 15:04:05", sessionInfo.ExpireTime, time.Local)
+		if err != nil {
+			continue
+		}
+		if time.Now().Unix() >= expireTime.Unix() {
+			func() {
+				// session过期
+				// 加分布式锁
+				lock, _ := server.NewLocker(key)
+				lock.Lock()
+				defer lock.Unlock()
+				// 删除session信息
+				_ = server.Delete(key)
+			}()
+		}
+	}
+}
+
+// 捕获异常信号，清理资源
+func release(s *Session, c chan os.Signal) {
+	<-c
+	_ = s.Close()
+	os.Exit(1)
+}
